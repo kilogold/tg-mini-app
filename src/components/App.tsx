f1f2300e6f70a8f1aadd12bc7821322cca5f0a58
kilogold <bonilla_kelvin@hotmail.com>
2024-11-19 21:29:49 -0400

    Test Wallet adapter

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import { AppRoot } from '@telegram-apps/telegram-ui';
 import { Navigate, Route, Routes, HashRouter } from 'react-router-dom';
 
 import { routes } from '@/navigation/routes.tsx';
+import { SolanaProvider } from './SolanaProvider';
 
 export function App() {
   const lp = useLaunchParams();
@@ -13,12 +14,14 @@ export function App() {
       appearance={isDark ? 'dark' : 'light'}
       platform={['macos', 'ios'].includes(lp.platform) ? 'ios' : 'base'}
     >
-      <HashRouter>
-        <Routes>
-          {routes.map((route) => <Route key={route.path} {...route} />)}
-          <Route path="*" element={<Navigate to="/"/>}/>
-        </Routes>
-      </HashRouter>
+      <SolanaProvider>
+        <HashRouter>
+          <Routes>
+            {routes.map((route) => <Route key={route.path} {...route} />)}
+            <Route path="*" element={<Navigate to="/"/>}/>
+          </Routes>
+        </HashRouter>
+      </SolanaProvider>
     </AppRoot>
   );
 }
---
+++ src/components/SolanaProvider.tsx
@@ -0,0 +1,49 @@
+import { useMemo } from 'react';
+import type { FC, ReactNode } from 'react';
+import {
+  ConnectionProvider,
+  WalletProvider
+} from '@solana/wallet-adapter-react';
+import {
+  WalletAdapterNetwork
+} from '@solana/wallet-adapter-base';
+import {
+  UnsafeBurnerWalletAdapter
+} from '@solana/wallet-adapter-wallets';
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+interface SolanaProviderProps {
+  children: ReactNode;
+}
+
+export const SolanaProvider: FC<SolanaProviderProps> = ({ children }) => {
+  const network = WalletAdapterNetwork.Devnet;
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new UnsafeBurnerWalletAdapter(),
+    ],
+    [network]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButton />
+          <WalletDisconnectButton />
+          {children}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}; 
